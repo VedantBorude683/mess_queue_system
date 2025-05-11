b68adc9d89dab0e35a7898affd6e5aae03456052
VedantBorude683 <vedantborude77@gmail.com>
2025-05-11 10:16:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 // Serve the index.html file at root (/)
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname,"index.html"));
 });
 
 let queue = [];
